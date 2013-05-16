d100ea35fffa7fc667752fe22cee349e261bbe86
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-05-16 14:09:27 -0300

    Alteração application.rb

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module Cupom
     # Enable the asset pipeline
     config.assets.enabled = false
 
-    config.assets.initialize_on_precompile = false
+
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
