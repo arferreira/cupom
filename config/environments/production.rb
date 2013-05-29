7498ae923ff0b68c268fb457921e9c4552e80cd4
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-05-29 02:12:53 -0400

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,9 @@ Cupom::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Enable the asset pipeline
-  config.assets.enabled = false
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
