9f05aff5a1bbedf06be2931ecf6789aa88328db7
Antonio Ricardo <antonioricardo@ifollowagencia.com.br>
2013-05-30 02:26:40 -0400

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,8 +66,8 @@ namespace :deploy do
   desc "Stop Application"
   task :stop, :roles => :app, :except => { :no_release => true } do 
     # mata o serviço do unicorn passando o pid definido na linha 99
-    run "#{try_sudo} kill `cat #{unicorn_pid}`"
-   # run "if [ -e /var/www/cupom/shared/pids/unicorn.pid ]; then kill `cat /var/www/cupom/shared/pids/unicorn.pid`; fi;"
+   # run "#{try_sudo} kill `cat #{unicorn_pid}`"
+    run "if [ -e /var/www/cupom/shared/pids/unicorn.pid ]; then kill `cat /var/www/cupom/shared/pids/unicorn.pid`; fi;"
 
   end
   
